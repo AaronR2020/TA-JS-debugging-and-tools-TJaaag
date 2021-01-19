cd2b5a6f3e9514a21e8b5b305ad1977c301c14c9
Aaron Rebelo <beingbetter21@gmail.com>
2021-01-19 13:32:44 +0530

    done

--- block-BJaafq/code/script.js
+++ block-BJaafq/code/script.js
@@ -4,3 +4,111 @@ function multiply(numA,numB){
 function add(numA,numB=0){
     return numA+numB
 }
+//testing framework and assertion
+
+//function to compair values
+function expect(expected){
+    return {
+        isEqual:function(result){
+            if(expected!==result){
+                throw new Error(`invalid code: \nexpected value: ${expected}\nretrieved value: ${result}`);  
+            }
+        },
+        typeEqual:function(result){
+            if(typeof result==undefined){
+                throw new Error(`invalid code: \nexpected value: ${expected}\nretrieved value: ${result}`);  
+            }
+        },
+        missingVal:function(result){
+            if(isNaN(result)==isNaN(expected)){
+                throw new Error(` invalid code\n missing value should be taken care of`)
+            }
+        },
+     
+    }
+}
+//....................................................................................
+//function to test values
+function test(msg,cb){
+    try{
+        cb()
+        console.log(`${msg} seems to work fine` );
+    }
+    catch(error){
+//console.error(error);
+console.log(`${msg}`,error)
+    }
+}
+//....................................................................................
+//add....................................................................................
+
+//case1
+//test case to add values
+function testAddCalc(){
+    let result1=add(2,1);
+    let expected1=3;
+
+    let result=add(2,2);
+    let expected=4;
+//add test function
+expect(expected1).isEqual(result1)
+expect(expected).isEqual(result)
+}
+
+//case2
+//test case to see if one of the input is missing
+function testAddMissign(){
+    let result=add(1);
+    let Notexpected=NaN
+    expect(Notexpected).missingVal(result)
+}
+//case3
+//test case to see if one of the input is a string
+function testAddType(){
+    let result=add(2,2);
+    let expected=4
+    expect(expected).typeEqual(result)
+}
+//case3.1
+//test case to see if one of the input is a string
+
+//multiply....................................................................................
+//case 1
+//test case to multiply values
+function testMulCalc(){
+    let result1=multiply(2,3);
+    let expected1=3;
+//add test function
+expect(expected1).isEqual(result1)
+}
+//case2
+//test case to see if one of the input is missing
+function testMulMissign(){
+    let result=multiply(1);
+    let Notexpected=NaN
+    expect(isNan(Notexpected)).missingVal(result)
+}
+//case 3
+//input type should be number
+function testMulType(){
+    let result=multiply(1,"hello");
+    let expected=3
+    expect(expected).typeEqual(result)
+}
+
+
+
+//function call....................................................................................
+//test case add value
+console.group("ADD");
+test("\n\nadding two numbers\n",testAddCalc);
+test("\n\nadding only one argument to add\n",testAddMissign);
+test("\n\nCheck if values inputed are numbers or not \n",testAddType)
+
+console.group("MULTIPLY");
+//test case multiply value
+test("\n\nMUL: multiplying two numbers\n",testMulCalc);
+test("\n\nMUL: adding only one argument to multiply\n",testMulMissign)
+test("\n\nMUL: Check if values inputed are numbers or not \n",testMulType)
+
+
--- block-BJaafq/code/test.js
+++ block-BJaafq/code/test.js
@@ -1,47 +1,108 @@
-function mul(numA, numB) {
-    return numA * numB;
-}
+//testing framework and assertion
 
-function add(numA, numB) {
-    return numA + numB;
-}
-
-function test(message, callback){
-    try{
-        callback();
-        console.log('✅', message);
-    } catch(error) {
-        console.error(error);
-        console.log('❌', message);
-    }
-}
-
-function expect(actual) {
+//function to compair values
+function expect(expected){
     return {
-        toEqual: function(expected) {
-            if(actual !== expected) {
-                throw new Error(`${actual} is not equal to ${expected}`);
+        isEqual:function(result){
+            if(expected!==result){
+                throw new Error(`invalid code: \nexpected value: ${expected}\nretrieved value: ${result}`);  
             }
         },
-
-       toBeEqual: function(expected) {
-        if(typeof actual !== expected) {
-            throw new Error(`${typeof actual} is not equal to ${expected}`);
-        }
-    }         
+        typeEqual:function(result){
+            if(typeof result==undefined){
+                throw new Error(`invalid code: \nexpected value: ${expected}\nretrieved value: ${result}`);  
+            }
+        },
+        missingVal:function(result){
+            if(isNaN(result)==isNaN(expected)){
+                throw new Error(` invalid code\n missing value should be taken care of`)
+            }
+        },
+     
     }
 }
+//....................................................................................
+//function to test values
+function test(msg,cb){
+    try{
+        cb()
+        console.log(`${msg} seems to work fine` );
+    }
+    catch(error){
+//console.error(error);
+console.log(`${msg}`,error)
+    }
+}
+//....................................................................................
+//add....................................................................................
 
-test('Add 10 and 10 to give a number', () => expect(add(10, 10)).toBeEqual(20));
-test('Add 10 and 30 to give a number', () => expect(add(10, 10)).toBeEqual(40));
-test('Add 10 and alt to give a number', () => expect(add(10)).toBeEqual(isNaN(NaN)));
-test('Add 10 and true to give a number', () => expect(add(10, true)).toBeEqual(11));
-test('Add 10 and undefined to give a number', () => expect(add(10, undefined)).toBeEqual(isNaN(NaN)));
+//case1
+//test case to add values
+function testAddCalc(){
+    let result1=add(2,1);
+    let expected1=3;
+
+    let result=add(2,2);
+    let expected=4;
+//add test function
+expect(expected1).isEqual(result1)
+expect(expected).isEqual(result)
+}
+
+//case2
+//test case to see if one of the input is missing
+function testAddMissign(){
+    let result=add(1);
+    let Notexpected=NaN
+    expect(Notexpected).missingVal(result)
+}
+//case3
+//test case to see if one of the input is a string
+function testAddType(){
+    let result=add(2,2);
+    let expected=4
+    expect(expected).typeEqual(result)
+}
+//case3.1
+//test case to see if one of the input is a string
+
+//multiply....................................................................................
+//case 1
+//test case to multiply values
+function testMulCalc(){
+    let result1=multiply(2,3);
+    let expected1=3;
+//add test function
+expect(expected1).isEqual(result1)
+}
+//case2
+//test case to see if one of the input is missing
+function testMulMissign(){
+    let result=multiply(1);
+    let Notexpected=NaN
+    expect(isNan(Notexpected)).missingVal(result)
+}
+//case 3
+//input type should be number
+function testMulType(){
+    let result=multiply(1,"hello");
+    let expected=3
+    expect(expected).typeEqual(result)
+}
 
 
 
-test('Multiply 10 and 10 to give a number', () => expect(mul(10, 10)).toBeEqual("number"));
-test('Multiply 10 and 30 to give a number', () => expect(mul(10, 30)).toBeEqual("number"));
-test('Multiply 10 and alt to give a number', () => expect(mul(10, 'alt')).toBeEqual("number"));
-test('Multiply 10 and true to give a number', () => expect(mul(10, true)).toBeEqual("number"));
-test('Multiply 10 and undefined to give a number', () => expect(mul(10, undefined)).toBeEqual("number"));
+//function call....................................................................................
+//test case add value
+console.group("ADD");
+test("\n\nadding two numbers\n",testAddCalc);
+test("\n\nadding only one argument to add\n",testAddMissign);
+test("\n\nCheck if values inputed are numbers or not \n",testAddType)
+
+console.group("MULTIPLY");
+//test case multiply value
+test("\n\nMUL: multiplying two numbers\n",testMulCalc);
+test("\n\nMUL: adding only one argument to multiply\n",testMulMissign)
+test("\n\nMUL: Check if values inputed are numbers or not \n",testMulType)
+
+
