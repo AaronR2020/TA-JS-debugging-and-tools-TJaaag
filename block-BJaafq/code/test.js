12be205ade7dfe4770e612a7dfff7aaf1b81e217
Aaron Rebelo <beingbetter21@gmail.com>
2021-01-19 13:19:23 +0530

    done

--- block-BJaafq/code/test.js
+++ block-BJaafq/code/test.js
@@ -1,113 +1,47 @@
-//testing framework and assertion
-
-//function to compair values
-function expect(expected){
-    return {
-        isEqual:function(result){
-            if(expected!==result){
-                throw new Error(`invalid code: \nexpected value: ${expected}\nretrieved value: ${result}`);  
-            }
-        },
-        typeEqual:function(result){
-            if(typeof expected!==typeof result){
-                throw new Error(`invalid code: \nexpected value: ${expected}\nretrieved value: ${result}`);  
-            }
-        },
-        missingVal:function(result){
-            if(isNaN(result)==isNaN(expected)){
-                throw new Error(` invalid code\n missing value should be taken care of`)
-            }
-        },
-        isEqualNumber:function(result){
-            if(typeof(result)!==typeof(2)){
-                throw new Error('the value inputed are not numbers')
-            }
-        }
-    }
+function mul(numA, numB) {
+    return numA * numB;
 }
-//....................................................................................
-//function to test values
-function test(msg,cb){
+
+function add(numA, numB) {
+    return numA + numB;
+}
+
+function test(message, callback){
     try{
-        cb()
-        console.log(`${msg} seems to work fine` );
+        callback();
+        console.log('✅', message);
+    } catch(error) {
+        console.error(error);
+        console.log('❌', message);
     }
-    catch(error){
-//console.error(error);
-console.log(`${msg}`,error)
+}
+
+function expect(actual) {
+    return {
+        toEqual: function(expected) {
+            if(actual !== expected) {
+                throw new Error(`${actual} is not equal to ${expected}`);
+            }
+        },
+
+       toBeEqual: function(expected) {
+        if(typeof actual !== expected) {
+            throw new Error(`${typeof actual} is not equal to ${expected}`);
+        }
+    }         
     }
 }
-//....................................................................................
-//add....................................................................................
 
-//case1
-//test case to add values
-function testAddCalc(){
-    let result1=add(2,1);
-    let expected1=3;
-
-    let result=add(2,2);
-    let expected=4;
-//add test function
-expect(expected1).isEqual(result1)
-expect(expected).isEqual(result)
-}
-
-//case2
-//test case to see if one of the input is missing
-function testAddMissign(){
-    let result=add(1);
-    let Notexpected=NaN
-    expect(Notexpected).missingVal(result)
-}
-//case3
-//test case to see if one of the input is a string
-function testAddType(){
-    let result=add("hello","nelllo");
-    let expected=typeof(2)
-    expect(expected).isEqualNumber(typeof(result))
-}
-//case3.1
-//test case to see if one of the input is a string
-
-//multiply....................................................................................
-//case 1
-//test case to multiply values
-function testMulCalc(){
-    let result1=multiply(2,3);
-    let expected1=6;
-//add test function
-expect(expected1).isEqual(result1)
-}
-//case2
-//test case to see if one of the input is missing
-function testMulMissign(){
-    let result=multiply(1);
-    let Notexpected=NaN
-    expect(isNan(Notexpected)).missingVal(result)
-}
-//case 3
-//input type should be number
-function testMulType(){
-    let result=multiply(1,"hello");
-    let expected=typeof(3)
-    expect(expected).isEqualNumber(typeof(result))
-}
+test('Add 10 and 10 to give a number', () => expect(add(10, 10)).toBeEqual(20));
+test('Add 10 and 30 to give a number', () => expect(add(10, 10)).toBeEqual(40));
+test('Add 10 and alt to give a number', () => expect(add(10)).toBeEqual(isNaN(NaN)));
+test('Add 10 and true to give a number', () => expect(add(10, true)).toBeEqual(11));
+test('Add 10 and undefined to give a number', () => expect(add(10, undefined)).toBeEqual(isNaN(NaN)));
 
 
 
-//function call....................................................................................
-//test case add value
-test("\n\nadding two numbers\n",testAddCalc);
-test("\n\nadding only one argument to add\n",testAddMissign);
-test("\n\nCheck if values inputed are numbers or not \n",testAddType)
-test("\n\nadding only one argument to add\n",testAddMissign);
-test("\n\nCheck if values inputed are numbers or not \n",testAddType)
-
-//test case multiply value
-test("\n\nmultiplying two numbers\n",testMulCalc);
-test("\n\nadding only one argument to multiply\n",testMulMissign)
-test("\n\nCheck if values inputed are numbers or not \n",testMulType)
-test("\n\nadding only one argument to multiply\n",testMulMissign)
-test("\n\nCheck if values inputed are numbers or not \n",testMulType)
-
+test('Multiply 10 and 10 to give a number', () => expect(mul(10, 10)).toBeEqual("number"));
+test('Multiply 10 and 30 to give a number', () => expect(mul(10, 30)).toBeEqual("number"));
+test('Multiply 10 and alt to give a number', () => expect(mul(10, 'alt')).toBeEqual("number"));
+test('Multiply 10 and true to give a number', () => expect(mul(10, true)).toBeEqual("number"));
+test('Multiply 10 and undefined to give a number', () => expect(mul(10, undefined)).toBeEqual("number"));
