ed694797c5b68738b7e51f0a5c0ea066c8d129b0
Aaron Rebelo <beingbetter21@gmail.com>
2021-01-19 13:03:53 +0530

    done

--- block-BJaafq/code/test.js
+++ block-BJaafq/code/test.js
@@ -1,3 +1,5 @@
+//testing framework and assertion
+
 //function to compair values
 function expect(expected){
     return {
@@ -14,6 +16,11 @@ function expect(expected){
         missingVal:function(result){
             if(isNaN(result)==isNaN(expected)){
                 throw new Error(` invalid code\n missing value should be taken care of`)
+            }
+        },
+        isEqualNumber:function(result){
+            if(typeof(result)!==typeof(2)){
+                throw new Error('the value inputed are not numbers')
             }
         }
     }
@@ -36,10 +43,14 @@ console.log(`${msg}`,error)
 //case1
 //test case to add values
 function testAddCalc(){
-let result1=add(2,1);
-let expected1=4;
+    let result1=add(2,1);
+    let expected1=3;
+
+    let result=add(2,2);
+    let expected=4;
 //add test function
 expect(expected1).isEqual(result1)
+expect(expected).isEqual(result)
 }
 
 //case2
@@ -49,6 +60,16 @@ function testAddMissign(){
     let Notexpected=NaN
     expect(Notexpected).missingVal(result)
 }
+//case3
+//test case to see if one of the input is a string
+function testAddType(){
+    let result=add("hello","nelllo");
+    let expected=typeof(2)
+    expect(expected).isEqualNumber(typeof(result))
+}
+//case3.1
+//test case to see if one of the input is a string
+
 //multiply....................................................................................
 //case 1
 //test case to multiply values
@@ -63,15 +84,26 @@ expect(expected1).isEqual(result1)
 function testMulMissign(){
     let result=multiply(1);
     let Notexpected=NaN
-    expect(Notexpected).missingVal(result)
+    expect(isNan(Notexpected)).missingVal(result)
+}
+//case 3
+//input type should be number
+function testMulType(){
+    let result=multiply(1,"hello");
+    let expected=typeof(3)
+    expect(expected).isEqualNumber(typeof(result))
 }
 
 
 
 //function call....................................................................................
 //test case add value
-test("adding two numbers\n",testAddCalc);
-test("adding only one argument to add\n",testAddMissign);
+test("\n\nadding two numbers\n",testAddCalc);
+test("\n\nadding only one argument to add\n",testAddMissign);
+test("\n\nCheck if values inputed are numbers or not \n",testAddType)
+
 //test case multiply value
-test("multiplying two numbers\n",testMulCalc);
-test("adding only one argument to multiply\n",testMulMissign)
+test("\n\nmultiplying two numbers\n",testMulCalc);
+test("\n\nadding only one argument to multiply\n",testMulMissign)
+test("\n\nCheck if values inputed are numbers or not \n",testMulType)
+
